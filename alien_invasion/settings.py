5ee0c4b6d6973aa15507a765d83e6d1979c96ba4
LucBourrat1 <luc.bourrat@gmail.com>
2024-04-12 20:25:32 +0200

    create aliens

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # screen settings
-        self.screen_width = 2400
+        self.screen_width = 2000
         self.screen_height = 1400
         self.bg_color = (230, 230, 230)
         # ship settings
@@ -14,4 +14,9 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 10
+        # alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right, -1 represents left
+        self.fleet_direction = 1
